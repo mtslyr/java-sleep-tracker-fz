28497a85743b8c1a6c468f89fd8eb6514db0f561
Mikhail Tsilyurik <mtsilyurik@yandex.ru>
2026-02-02 20:17:30 +0500

    Исправления по CheckStyle / 0

--- src/main/java/ru/yandex/practicum/sleeptracker/analysis/func/MinSessionDurationFunc.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/analysis/func/MinSessionDurationFunc.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.sleeptracker.analysis.func;
 import ru.yandex.practicum.sleeptracker.analysis.SleepAnalysisResult;
 import ru.yandex.practicum.sleeptracker.sleep.SleepSession;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
